b77dc4f9cacdc68a9bb1c8d3917a14e4c011daa2
GeorgiaC <georgiabc92@gmail.com>
2018-03-24 15:25:15 -0400

    depth first planner update

--- hw3/code/DepthFirstPlanner.py
+++ hw3/code/DepthFirstPlanner.py
@@ -35,11 +35,14 @@ class DepthFirstPlanner(object):
         idx = 1 #order a node was visited in
         Q = deque([root_id]) #queue of nodes to expand
 
-        #create an array of nodes and give visited nodes their idx
+        # #create an array of nodes and give visited nodes their idx
         width = self.planning_env.discrete_env.num_cells[0]
         height = self.planning_env.discrete_env.num_cells[1]
         visit = np.zeros(width*height)
         visit[root_id] = 1
+        self.nodes[start_id] = {}
+
+        arrived = 0
 
         while Q :
 
@@ -47,8 +50,6 @@ class DepthFirstPlanner(object):
             #take last node in list
             next_node = Q.pop()
 
-            if next_node == goal_id:
-                break
 
             successors = self.planning_env.GetSuccessors(next_node)
             # print successors
@@ -59,9 +60,10 @@ class DepthFirstPlanner(object):
                     #Mark visited node with it's index
                     idx += 1
                     visit[id] = idx
-                    # #set resolution to 1 to view grid
-                    # print np.reshape(visit, (10, 10))
-                    #add new nodes to queue
+
+                    #add node's mother to dictionary and add node to queue
+                    self.nodes[id] = next_node
+                    # print self.nodes
                     Q.append(id) 
 
                     #plot edges
@@ -69,32 +71,34 @@ class DepthFirstPlanner(object):
                     id_coord = self.planning_env.discrete_env.NodeIdToConfiguration(id)
                     self.planning_env.PlotEdge(node_coord, id_coord)
 
+                    if id == goal_id:
+                        arrived = 1
+
+            #if goal is reached, begin planning
+            if arrived:
+                break
+
 
         #Initialize plan
         plan = goal_config
         prev_node = goal_id
 
-        best_idx = 10000
-        prev_visit = 10000
-
         while True:
 
-            #amoung a node's successors, find the one with the smalles idx
-            successors = self.planning_env.GetSuccessors(prev_node)
-            for id in successors:
-                if visit[id] < prev_visit and visit[id] != 0:
-                    best_idx = id
-                    prev_visit = visit[id]
+            #break if you've reached the starting point
+            if prev_node == start_id:
+                break
 
-            #add smallest idx node to plan
-            prev_node = best_idx
-            print prev_visit
+            #find current node's mother node and set prev_node to mother
+            mother = self.nodes[prev_node]
+            prev_node = mother
+
+            #add mother node to plan
             prev_config = self.planning_env.discrete_env.NodeIdToConfiguration(prev_node)
 
             plan = np.vstack((prev_config, plan))
 
-            if best_idx == start_id:
-                break
+            
 
         
         plan = np.vstack((start_config, plan))

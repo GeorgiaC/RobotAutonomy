d1851cd0455a42d5e20362abef54801b60cf9598
Chris Song <songqian2002@gmail.com>
2018-04-17 22:04:59 -0400

    more progress with grasping

--- hw4/code/GraspPlanner.py
+++ hw4/code/GraspPlanner.py
@@ -1,5 +1,6 @@
-import logging, numpy, openravepy
+import logging, openravepy
 import pdb
+import numpy as np
 
 class GraspPlanner(object):
 
@@ -8,7 +9,7 @@ class GraspPlanner(object):
         self.base_planner = base_planner
         self.arm_planner = arm_planner
 
-            
+
     def GetBasePoseForObjectGrasp(self, obj):
 
         # Load grasp database
@@ -16,21 +17,21 @@ class GraspPlanner(object):
         self.grasps = self.gmodel.grasps
         self.grasp_indices = self.gmodel.graspindices
 
-        if not gmodel.load():
-            gmodel.autogenerate()
+        if not self.gmodel.load():
+            self.gmodel.autogenerate()
 
         base_pose = None
         grasp_config = None
-       
+
         ###################################################################
         # TODO: Here you will fill in the function to compute
-        #  a base pose and associated grasp config for the 
+        #  a base pose and associated grasp config for the
         #  grasping the bottle
         ###################################################################
 
         # base_pose, position of herb's body
         # grasp_config 7-DOF arm config
-        order_grasps()
+        self.order_grasps()
 
         pdb.set_trace()
 
@@ -46,7 +47,7 @@ class GraspPlanner(object):
             exit()
 
         # Now plan to the base pose
-        start_pose = numpy.array(self.base_planner.planning_env.herb.GetCurrentConfiguration())
+        start_pose = np.array(self.base_planner.planning_env.herb.GetCurrentConfiguration())
         base_plan = self.base_planner.Plan(start_pose, base_pose)
         base_traj = self.base_planner.planning_env.herb.ConvertPlanToTrajectory(base_plan)
 
@@ -54,7 +55,7 @@ class GraspPlanner(object):
         self.base_planner.planning_env.herb.ExecuteTrajectory(base_traj)
 
         # Now plan the arm to the grasp configuration
-        start_config = numpy.array(self.arm_planner.planning_env.herb.GetCurrentConfiguration())
+        start_config = np.array(self.arm_planner.planning_env.herb.GetCurrentConfiguration())
         arm_plan = self.arm_planner.Plan(start_config, grasp_config)
         arm_traj = self.arm_planner.planning_env.herb.ConvertPlanToTrajectory(arm_plan)
 
@@ -66,10 +67,10 @@ class GraspPlanner(object):
         task_manipultion.CloseFingers()
 
     def order_grasps(self):
-        self.grasps_ordered = self.grasps.copy() #you should change the order of self.grasps_ordered
+        self.grasps_ordered = np.asarray(self.grasps).copy
         for grasp in self.grasps_ordered:
             grasp[self.graspindices.get('performance')] = self.eval_grasp(grasp)
-            
+
         # sort!
         order = np.argsort(self.grasps_ordered[:,self.graspindices.get('performance')[0]])
         order = order[::-1]
@@ -79,21 +80,18 @@ class GraspPlanner(object):
         with self.robot:
             #contacts is a 2d array, where contacts[i,0-2] are the positions of contact i and contacts[i,3-5] is the direction
             try:
-                contacts,finalconfig,mindist,volume = self.gmodel.testGrasp(grasp=grasp,translate=True,forceclosure=False)
-
+                contacts, finalconfig, mindist, volume = self.gmodel.testGrasp(grasp=grasp,
+                                                                               translate=True,
+                                                                               forceclosure=False)
                 obj_position = self.gmodel.target.GetTransform()[0:3,3]
-                # for each contact
                 G = np.array([]) #the wrench matrix
-
                 arr = np.empty([6, len(contacts)])
                 count = 0
+
                 for c in contacts:
                     pos = c[0:3] - obj_position
                     dir = -c[3:] #this is already a unit vector
-                    
-                    ##############
-                    # start TODO #
-                    ##############
+
                     p = np.transpose(pos)
                     p = np.reshape(p, (3, 1))
                     n = np.transpose(dir)
@@ -107,15 +105,17 @@ class GraspPlanner(object):
 
                     arr[:, count] = fill
                     count = count + 1
-                    # end TODO
 
-                # start TODO
                 G = arr
                 eigvals = np.sqrt(LA.eigvals(G.dot(np.transpose(G))))
                 Q1 = np.min(eigvals)
                 Q2 = np.sqrt(LA.det(G.dot(np.transpose(G))))
-                ############
-                # end TODO #
-                ############
+                return Q1
 
-                return Q1 #change this
+            except openravepy.planning_error, e:
+                return 0.00
+            except np.LinAlgError:
+                return 0.00
+
+    # def eval_graps(self, grasp):
+
